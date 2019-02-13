47b20e4daa4fd03920cfc0c44ee7c591d9a419b7
Ali Shokoohi <alishokoohi666@gmail.com>
2019-02-13 17:12:43 +0330

    Any url must controling by web/url.py except admin/

--- omrun/urls.py
+++ omrun/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('web/', include('web.urls')),
+    path('/', include('web.urls')),
     path('admin/', admin.site.urls),
 ]
