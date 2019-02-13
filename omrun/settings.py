6d39520a0607326ef5bd847d5bd9812aad477753
Ali Shokoohi <alishokoohi666@gmail.com>
2019-02-13 23:10:08 +0330

    Add web app tp ISNTALLED_APPS

--- omrun/settings.py
+++ omrun/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'web',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
