6a4bc11bcf38abff63716dfc7b1b4a47602e4e5d
Ali Shokoohi <alishokoohi666@gmail.com>
2019-02-18 15:46:12 +0330

    Add class Employees model

--- web/models.py
+++ web/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Employees(models.Model):
+    name = models.TextField(null=False)
+    personnelـid = models.IntegerField(max_length=16, null=False, primary_key=True)
+    password = models.CharField(max_lenght=32, null=False)
+    email = models.CharField(max_lenght=32)
