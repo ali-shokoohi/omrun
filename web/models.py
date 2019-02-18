4526bad82bc65189d8119c3afb99a258a2dfab7d
Ali Shokoohi <alishokoohi666@gmail.com>
2019-02-18 16:10:43 +0330

    Change Employees password max_lenght to 64 for hashes

--- web/models.py
+++ web/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Employees(models.Model):
     name = models.TextField(null=False)
     personnelـid = models.BigIntegerField(max_length=16, null=False, primary_key=True)
-    password = models.CharField(max_lenght=32, null=False)
+    password = models.CharField(max_lenght=64, null=False)
     email = models.CharField(max_lenght=32)
     profile_pic = models.CharField(max_length=20)
 
