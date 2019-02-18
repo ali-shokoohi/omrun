e079bc8dab9c1f3b523706275ec7a15ae557e938
Ali Shokoohi <alishokoohi666@gmail.com>
2019-02-18 15:53:47 +0330

    Fix Employees model

--- web/models.py
+++ web/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 class Employees(models.Model):
     name = models.TextField(null=False)
-    personnelـid = models.IntegerField(max_length=16, null=False, primary_key=True)
+    personnelـid = models.BigIntegerField(max_length=16, null=False, primary_key=True)
     password = models.CharField(max_lenght=32, null=False)
     email = models.CharField(max_lenght=32)
+
+    def __str__(self):
+        return self.name
