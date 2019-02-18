fa0815a129150255f35f9c6d57ffc37f9010dcd8
Ali Shokoohi <alishokoohi666@gmail.com>
2019-02-18 16:14:48 +0330

    Change max_lenght to max_length

--- web/models.py
+++ web/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class Employees(models.Model):
     name = models.TextField(null=False)
     personnelـid = models.BigIntegerField(max_length=16, null=False, primary_key=True)
-    password = models.CharField(max_lenght=64, null=False)
-    email = models.CharField(max_lenght=32)
+    password = models.CharField(max_length=64, null=False)
+    email = models.CharField(max_length=32)
     profile_pic = models.CharField(max_length=20)
 
     def __str__(self):
