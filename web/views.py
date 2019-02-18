6718ff50f3388b47b25fd4b9837fc155a415291e
Ali Shokoohi <alishokoohi666@gmail.com>
2019-02-18 14:23:02 +0330

    Working with static files

--- web/views.py
+++ web/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request=request, template_name="index/index.html")
 
