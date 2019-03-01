fe816f9be5dd0d0f2390d4a53da6d99654c02cb4
Ali Shokoohi <alishokoohi666@gmail.com>
2019-03-01 22:45:38 +0330

    Update settings.py

--- omrun/settings.py
+++ omrun/settings.py
@@ -108,7 +108,7 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.JSONParser',
     )
