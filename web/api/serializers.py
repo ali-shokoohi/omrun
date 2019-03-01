229291507c2eb54c24817154d5e473e3f6c463dc
Ali Shokoohi <alishokoohi666@gmail.com>
2019-03-02 01:30:31 +0330

    Set fields to Purchases serializer

--- web/api/serializers.py
+++ web/api/serializers.py
@@ -4,6 +4,7 @@ from ..models import Purchases, Comments
 class Purchases_serializers(serializers.ModelSerializer):
     class Meta:
         model = Purchases
+        fields = ("buyer", "amount", "for_what", "date")
 
 class Comments_serializers(serializers.ModelSerializer):
     class Meta:
