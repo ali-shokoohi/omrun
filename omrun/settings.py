e9393d442b60b337c1f5ca0c4d6851160a177696
Ali Shokoohi <alishokoohi666@gmail.com>
2019-03-01 22:40:58 +0330

    Set JSONParser as Default parser

--- omrun/settings.py
+++ omrun/settings.py
@@ -109,6 +109,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+    )
 }
 
 # Internationalization
