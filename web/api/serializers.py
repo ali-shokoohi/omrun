78fc4d930741cc023498e79dfb443539dbc7ed03
Ali Shokoohi <alishokoohi666@gmail.com>
2019-03-02 01:37:31 +0330

    write comments!

--- web/api/serializers.py
+++ web/api/serializers.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers
 from ..models import Purchases, Comments
 
+#Serializer of Purchases model
 class Purchases_serializers(serializers.ModelSerializer):
     class Meta:
         model = Purchases
         fields = ("buyer", "amount", "for_what", "date")
 
+#Serializer of Purchases model
 class Comments_serializers(serializers.ModelSerializer):
     class Meta:
         model = Comments
