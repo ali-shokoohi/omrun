38827bd1a13ced74485f429ab8cd9d237642da25
Ali Shokoohi <alishokoohi666@gmail.com>
2019-03-07 19:08:05 +0330

    Use rest auth&authtoken

--- omrun/settings.py
+++ omrun/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'web',
 ]
 
@@ -111,6 +112,10 @@ REST_FRAMEWORK = {
 #    ],
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.JSONParser',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     )
 }
 
