0d74ffb52893f6f7ddc539bb36f44ca0f5e60a41
Ali Shokoohi <alishokoohi666@gmail.com>
2019-03-12 00:11:35 +0330

    Read SECRET_KEY from secrets.py FIXED!

--- omrun/settings.py
+++ omrun/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 #Import SECRET_KEY from secrets.py
-from secrets import SECRET_KEY
+from .secrets import SECRET_KEY
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
