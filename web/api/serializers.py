261bce691fe6193ea4a1220415dc45579de30903
Ali Shokoohi <alishokoohi666@gmail.com>
2019-04-30 10:06:05 +0430

    Add plans serializer

--- web/api/serializers.py
+++ web/api/serializers.py
@@ -1,11 +1,16 @@
 from rest_framework import serializers
-from web.models import Purchases, Comments, Projects
+from web.models import Purchases, Comments, Projects, Plans
 
 #Serializer of Purchases model
 class Projects_Serializers(serializers.ModelSerializer):
     class Meta:
         model = Projects
         fields = ("id", "name", "employer")
+
+class PLans_Serializers(serializers.ModelSerializer):
+    class Meta:
+        model = Plans
+        fields = ("photo", "data", "kind")
 
 #Serializer of Purchases model
 class Purchases_serializers(serializers.ModelSerializer):
