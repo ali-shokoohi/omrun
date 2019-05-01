8a7be968831911252efc909c1128a4673543a84d
Ali Shokoohi <alishokoohi666@gmail.com>
2019-05-02 02:22:13 +0430

    Add Token&User&Employees serializers

--- web/api/serializers.py
+++ web/api/serializers.py
@@ -1,8 +1,33 @@
 from rest_framework import serializers
-from web.models import Purchases, Comments, Projects, Plans
+from web.models import Purchases, Comments, Projects, Plans, User, Employees
+from rest_framework.authtoken.models import Token
 
+#Serializer of Token model
+class Token_Serializers(serializers.ModelSerializer):
+    class Meta:
+        model = Token
+        fileds = ("key",)
+
+#Serializer of User model
+class User_Serializers(serializers.ModelSerializer):
+    #token = Token_Serializers(required=True)
+    class Meta:
+        model = User
+        fields = ("first_name", "last_name", "username", "email")
+        #TODO: Add token field also ...
+
+
+#Serializer of Employees
+class Employees_Serializers(serializers.ModelSerializer):
+    user = User_Serializers(required=True)
+
+    class Meta:
+        model = Employees
+        fields = ("user", "post", "profile_pic")
 #Serializer of Purchases model
 class Projects_Serializers(serializers.ModelSerializer):
+    employer = Employees_Serializers(required=True)
+
     class Meta:
         model = Projects
         fields = ("id", "name", "employer")
