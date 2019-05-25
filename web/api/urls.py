5db512beb124a802c5282b7d694b596f9646de30
Ali Shokoohi <alishokoohi666@gmail.com>
2019-05-25 20:27:12 +0430

    Soma changes and migrates

--- web/api/urls.py
+++ web/api/urls.py
@@ -8,6 +8,12 @@ urlpatterns = [
     path('projects/', views.projects_list.as_view(), name='projects list'),
     path('projects/<int:pk>/', views.projects_detial.as_view(), name='project detail'),
     path('plans/', views.plans_list.as_view(), name='plans list'),
-    path('plans/<int:pk>/', views.plans_detail.as_view(), name='plan detail')
+    path('plans/<int:pk>/', views.plans_detail.as_view(), name='plan detail'),
+    path('tasks/', views.Tasks_list.as_view(), name='projects list'),
+    path('tasks/<int:pk>/', views.Tasks_detial.as_view(), name='project detail'),
+    path('todo/', views.ToDo_list.as_view(), name='plans list'),
+    path('todo/<int:pk>/', views.ToDo_detail.as_view(), name='plan detail'),
+    path('projects/delete/<int:pk>/', views.Project_delete.as_view(), name='project delete'),
+    path('plans/delete/<int:pk>/', views.Plan_delete.as_view(), name='plan delete')
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
